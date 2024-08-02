02f3d789f19e4cf7f72d03f9c56ce95cbdee712f
Adam <adam@phenaproxima.net>
2024-08-02 16:26:02 -0400

    Add test coverage to prove that the installer actually applies the Starshot recipe (#163)

--- tests/src/ExistingSite/BasicExpectationsTest.php
+++ tests/src/ExistingSite/BasicExpectationsTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\starshot\ExistingSite;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ExtensionList;
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Core\Extension\ThemeExtensionList;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
@@ -22,9 +24,31 @@ class BasicExpectationsTest extends ExistingSiteBase {
     $assert_session->statusCodeEquals(200);
     $assert_session->elementAttributeContains('css', 'meta[name="Generator"]', 'content', 'Drupal');
 
-    /** @var \Drupal\Core\Config\ConfigFactoryInterface $config_factory */
-    $config_factory = $this->container->get(ConfigFactoryInterface::class);
-    $this->assertEmpty($config_factory->get('core.extension')->get('profile'));
+    // The installer should have uninstalled itself.
+    $this->assertFalse($this->container->getParameter('install_profile'));
+
+    // Ensure that there are non-core extensions installed, which proves that
+    // recipes were applied during site installation.
+    $this->assertContribInstalled($this->container->get(ModuleExtensionList::class));
+    $this->assertContribInstalled($this->container->get(ThemeExtensionList::class));
+  }
+
+  /**
+   * Asserts that any number of contributed extensions are installed.
+   *
+   * @param \Drupal\Core\Extension\ExtensionList $list
+   *   An extension list.
+   */
+  private function assertContribInstalled(ExtensionList $list): void {
+    $core_dir = $this->container->getParameter('app.root') . '/core';
+
+    foreach (array_keys($list->getAllInstalledInfo()) as $name) {
+      // If the extension isn't part of core, great! We're done.
+      if (!str_starts_with($list->getPath($name), $core_dir)) {
+        return;
+      }
+    }
+    $this->fail('No contributed extensions are installed.');
   }
 
 }
